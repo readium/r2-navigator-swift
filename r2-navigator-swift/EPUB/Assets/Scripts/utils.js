95010536c1e7078bb84ce0a093ae42f7f426e38f
Dimitar Marinov <di.marinov@gmail.com>
2021-06-08 15:37:29 +0300

    add snap to element function

--- r2-navigator-swift/EPUB/Assets/Scripts/utils.js
+++ r2-navigator-swift/EPUB/Assets/Scripts/utils.js
@@ -80,19 +80,54 @@ var readium = (function() {
     function scrollToId(id) {
         var element = document.getElementById(id);
         if (!element) {
-            return false;
+            return;
         }
-        element.scrollIntoView();
         
         if (!isScrollModeEnabled()) {
-            var currentOffset = window.scrollX;
-            var pageWidth = window.innerWidth;
-            // Adds half a page to make sure we don't snap to the previous page.
-            document.scrollingElement.scrollLeft = snapOffset(currentOffset + (pageWidth / 2));
+            snapToElement(element)
+        } else {
+            element.scrollIntoView();
         }
-        return true;
     }
 
+    function snapToElement(element) {
+        function offset(el) {
+            var rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+        }
+        
+        var pageWidth = window.innerWidth;
+        var scrollXBefore = Math.floor(window.scrollX);
+        
+        element.scrollIntoView();
+        
+        var currentOffset = Math.floor(window.scrollX);
+        var elementOffset = Math.floor(offset(element).left);
+        var rect = element.getBoundingClientRect();
+        var elementWidth = rect.right - rect.left;
+        
+         if (currentOffset < elementOffset) { // element is on the right
+             if (scrollXBefore < currentOffset) {
+                 document.scrollingElement.scrollLeft += pageWidth - (element.offsetLeft + elementWidth);
+             } else {
+                 var snap = document.scrollingElement.scrollLeft == 0; // check if this is a new section
+                 document.scrollingElement.scrollLeft += pageWidth - (element.offsetLeft);
+                 if (snap) { // snap when switch sections
+                     snapCurrentPosition();
+                 }
+
+             }
+         } else {
+             if (scrollXBefore < currentOffset) { // element is on the left
+                 document.scrollingElement.scrollLeft += pageWidth - (element.offsetLeft + elementWidth);
+             } else {
+                 document.scrollingElement.scrollLeft -= (element.offsetLeft);
+             }
+         }
+    }
+    
     // Position must be in the range [0 - 1], 0-100%.
     function scrollToPosition(position, dir) {
         console.log("ScrollToPosition");
@@ -110,122 +145,6 @@ var readium = (function() {
             var factor = (dir == 'rtl') ? -1 : 1;
             var offset = documentWidth * position * factor;
             document.scrollingElement.scrollLeft = snapOffset(offset);
-        }
-    }
-
-    // Scrolls to the first occurrence of the given text snippet.
-    //
-    // The expected text argument is a Locator Text object, as defined here:
-    // https://readium.org/architecture/models/locators/
-    function scrollToText(text) {
-        // Wrapper around a browser Selection object.
-        function Selection(selection) {
-            this.selection = selection;
-            this.markedRanges = []
-        }
-
-        // Removes all the ranges of the selection.
-        Selection.prototype.clear = function() {
-            this.selection.removeAllRanges();
-        }
-
-        // Saves the current selection ranges, to be restored later with reset().
-        Selection.prototype.mark = function() {
-            this.markedRanges = []
-            for (var i = 0; i < this.selection.rangeCount; i++) {
-                this.markedRanges.push(this.selection.getRangeAt(i));
-            }
-        }
-
-        // Resets the selection with ranges previously saved with mark().
-        Selection.prototype.reset = function() {
-            this.clear();
-            for (var i = 0; i < this.markedRanges.length; i++) {
-                this.selection.addRange(this.markedRanges[i]);
-            }
-        }
-
-        // Returns the text content of the selection.
-        Selection.prototype.toString = function() {
-            return this.selection.toString();
-        }
-
-        // Extends the selection by moving the start and end positions by the given offsets.
-        Selection.prototype.adjust = function(offset, length) {
-            for (var i = 0; i <= Math.abs(offset); i++) {
-                var direction = (offset >= 0 ? "forward" : "backward")
-                this.selection.modify("move", direction, "character");
-            }
-            for (var i = 0; i <= length; i++) {
-                this.selection.modify("extend", "forward", "character");
-            }
-        }
-
-        Selection.prototype.isEmpty = function() {
-            return this.selection.isCollapsed
-        }
-
-        Selection.prototype.getFirstRange = function() {
-            return this.selection.getRangeAt(0);
-        }
-
-        function removeWhitespaces(s) {
-            return s.replace(/\s+/g, "");
-        }
-
-        var highlight = text.highlight;
-        var before  = text.before || "";
-        var after  = text.after || "";
-        var snippet = before + highlight + after;
-        var safeSnippet = removeWhitespaces(snippet);
-
-        if (!highlight || !safeSnippet) {
-            return false;
-        }
-
-        var selection = new Selection(window.getSelection());
-        // We need to reset any selection to begin the search from the start of the resource.
-        selection.clear();
-
-        var found = false;
-        while (window.find(text.highlight, true)) {
-            if (selection.isEmpty()) {
-                break; // Prevents infinite loop in edge cases.
-            }
-
-            // Get the surrounding context to compare to the expected snippet.
-            selection.mark();
-            selection.adjust(-before.length, snippet.length);
-            var safeSelection = removeWhitespaces(selection.toString());
-            selection.reset();
-
-            if (safeSelection !== "" && (safeSnippet.includes(safeSelection) || safeSelection.includes(safeSnippet))) {
-                found = true;
-                break;
-            }
-        }
-
-        if (!found || selection.isEmpty()) {
-            return false;
-        }
-
-        // WKWebView doesn't seem to scroll to the selection created by window.find().
-        // See https://bugs.webkit.org/show_bug.cgi?id=163911
-        scrollToRange(selection.getFirstRange())
-
-        // Resets the selection otherwise the last found occurrence will be highlighted.
-        selection.clear();
-
-        return true;
-    }
-
-    function scrollToRange(range) {
-        var rect = range.getBoundingClientRect();
-        if (isScrollModeEnabled()) {
-            document.scrollingElement.scrollTop = rect.top + window.scrollY - (window.innerHeight / 2);
-        } else {
-            document.scrollingElement.scrollLeft = rect.left + window.scrollX;
-            snapCurrentPosition();
         }
     }
 
@@ -316,7 +235,6 @@ var readium = (function() {
     return {
         'scrollToId': scrollToId,
         'scrollToPosition': scrollToPosition,
-        'scrollToText': scrollToText,
         'scrollLeft': scrollLeft,
         'scrollRight': scrollRight,
         'setProperty': setProperty,
