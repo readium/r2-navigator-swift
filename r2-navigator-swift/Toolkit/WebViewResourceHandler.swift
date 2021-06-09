b583b090e072e287710779218fb24b8181d8fc90
Mickaël Menu <mickael.menu@gmail.com>
2021-06-09 09:25:47 +0200

    Fix deadlocks and crashes when using WKURLSchemeTask

--- r2-navigator-swift/Toolkit/WebViewResourceHandler.swift
+++ r2-navigator-swift/Toolkit/WebViewResourceHandler.swift
@@ -54,22 +54,26 @@ final class WebViewResourceHandler: NSObject, WKURLSchemeHandler, Loggable {
 
     private final class Task {
         private let url: URL
-        private let task: WKURLSchemeTask
         private let resource: Resource
         private var isCancelled = false
-        private let isCancelledQueue = DispatchQueue(label: "org.readium.r2-navigator-swift.WebViewResourceHandler.Task")
-
+        
+        /// Underlying WKURLSchemeTask.
+        /// Use `withTask()` to access it safely.
+        private let _task: WKURLSchemeTask
+        
         init(url: URL, task: WKURLSchemeTask, resource: Resource) {
             self.url = url
-            self.task = task
             self.resource = resource
+            self._task = task
         }
 
         func start() {
+            let request = _task.request
+            
             DispatchQueue.global(qos: .userInitiated).async {
                 let href = self.resource.link.href
                 do {
-                    log(.info, "Will serve \(href), headers: \(self.task.request.allHTTPHeaderFields ?? [:])")
+                    log(.info, "Will serve \(href), headers: \(request.allHTTPHeaderFields ?? [:])")
                     let length = try self.resource.length.get()
                     let response = URLResponse(
                         url: self.url,
@@ -77,9 +81,7 @@ final class WebViewResourceHandler: NSObject, WKURLSchemeHandler, Loggable {
                         expectedContentLength: Int(length),
                         textEncodingName: nil
                     )
-                    self.ifNotCancelled {
-                        self.task.didReceive(response)
-                    }
+                    self.withTask { $0.didReceive(response) }
 
                     var available = length
                     var offset: UInt64 = 0
@@ -91,20 +93,14 @@ final class WebViewResourceHandler: NSObject, WKURLSchemeHandler, Loggable {
                         offset += UInt64(data.count)
                         available -= UInt64(data.count)
 
-                        self.ifNotCancelled {
-                            self.task.didReceive(data)
-                        }
+                        self.withTask { $0.didReceive(data) }
                     } while available > 0 && !self.isCancelled
 
-                    self.ifNotCancelled {
-                        self.task.didFinish()
-                    }
+                    self.withTask { $0.didFinish() }
 
                 } catch {
                     log(.error, "Failed to serve \(href): \(error.localizedDescription)")
-                    self.ifNotCancelled {
-                        self.task.didFailWithError(error)
-                    }
+                    self.withTask { $0.didFailWithError(error) }
                 }
 
                 self.resource.close()
@@ -112,18 +108,21 @@ final class WebViewResourceHandler: NSObject, WKURLSchemeHandler, Loggable {
         }
 
         func cancel() {
-            ifNotCancelled {
-                isCancelled = true
-            }
+            assert(Thread.isMainThread)
+            self.isCancelled = true
         }
-
-        func ifNotCancelled(callback: () -> Void) {
-            isCancelledQueue.sync {
-                if !isCancelled {
-                    callback()
+        
+        /// Any API call to WKURLSchemeTask will crash the app if the task is cancelled by a call to:
+        /// `webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask)`.
+        ///
+        /// To prevent this, we need to synchronize on the main thread every API calls to the task.
+        private func withTask(callback: (WKURLSchemeTask) -> Void) {
+            assert(!Thread.isMainThread)
+            DispatchQueue.main.sync {
+                if !self.isCancelled {
+                    callback(self._task)
                 }
             }
         }
     }
-
 }
